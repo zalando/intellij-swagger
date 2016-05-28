e1c391044a71516206c815f29642d9a96a823a25
viatra2 <viatra2@gmail.com>
2016-05-28 10:33:53 +0200

    tests refactoring before fixing #25 (part 3 of 3)
    - field_ prefix for test data files under /field folder is now useless, removed

--- src/test/java/org/zalando/intellij/swagger/completion/InsertFieldTest.java
+++ src/test/java/org/zalando/intellij/swagger/completion/InsertFieldTest.java
@@ -35,31 +35,31 @@ public class InsertFieldTest {
 
     @Test
     public void thatStringFieldIsHandled() {
-        myFixture.complete("field_string", myJsonOrYaml);
-        myFixture.checkResultByFile("field_string_after", myJsonOrYaml);
+        myFixture.complete("string", myJsonOrYaml);
+        myFixture.checkResultByFile("string_after", myJsonOrYaml);
     }
 
     @Test
     public void thatObjectFieldIsHandled() {
-        myFixture.complete("field_object", myJsonOrYaml);
-        myFixture.checkResultByFile("field_object_after", myJsonOrYaml);
+        myFixture.complete("object", myJsonOrYaml);
+        myFixture.checkResultByFile("object_after", myJsonOrYaml);
     }
 
     @Test
     public void thatArrayFieldIsHandled() {
-        myFixture.complete("field_array", myJsonOrYaml);
-        myFixture.checkResultByFile("field_array_after", myJsonOrYaml);
+        myFixture.complete("array", myJsonOrYaml);
+        myFixture.checkResultByFile("array_after", myJsonOrYaml);
     }
 
     @Test
     public void testCommaAfterMidwayObject() {
-        myFixture.complete("field_object_midway", myJsonOrYaml);
-        myFixture.checkResultByFile("field_object_midway_after", myJsonOrYaml);
+        myFixture.complete("object_midway", myJsonOrYaml);
+        myFixture.checkResultByFile("object_midway_after", myJsonOrYaml);
     }
 
     @Test
     public void testCommaAfterMidwayArray() {
-        myFixture.complete("field_array_midway", myJsonOrYaml);
-        myFixture.checkResultByFile("field_array_midway_after", myJsonOrYaml);
+        myFixture.complete("array_midway", myJsonOrYaml);
+        myFixture.checkResultByFile("array_midway_after", myJsonOrYaml);
     }
 }
