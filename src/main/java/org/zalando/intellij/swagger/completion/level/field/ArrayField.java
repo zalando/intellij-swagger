dbc1863acae1cef1ee85cf5c6b58aec74cc117a6
Sebastian Monte <sebastian.h.monte@gmail.com>
2016-06-02 14:29:49 +0200

    Fixes #34

--- src/main/java/org/zalando/intellij/swagger/completion/level/field/ArrayField.java
+++ src/main/java/org/zalando/intellij/swagger/completion/level/field/ArrayField.java
@@ -20,9 +20,7 @@ public class ArrayField extends Field {
 
     @Override
     public String getYamlPlaceholderSuffix(final int indentation) {
-        final String indentationPadding = org.apache.commons.lang.StringUtils.repeat(" ", indentation);
         final String nextLineIndentationPadding = org.apache.commons.lang.StringUtils.repeat(" ", indentation + 2);
-
-        return ":\n" + "  " + "- <caret>";
+        return ":\n" + nextLineIndentationPadding + "- <caret>";
     }
 }
