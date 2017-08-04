bdb1c4f5fbfd4b9080a7139f8cc5b05b117cde59
Sebastian Monte <sebastian.monte@zalando.de>
2017-08-04 13:45:22 +0200

    Added OpenAPI file detector
    We can no longer detect Swagger files just be looking at filename,
    but instead check spec for keys "swagger" and "openapi".

--- src/main/java/org/zalando/intellij/swagger/file/FileConstants.java
+++ src/main/java/org/zalando/intellij/swagger/file/FileConstants.java
@@ -2,18 +2,13 @@ package org.zalando.intellij.swagger.file;
 
 public class FileConstants {
 
-    public static final String JSON_FILE_EXTENSION = "json";
-    public static final String YML_FILE_EXTENSION = "yml";
-    public static final String YAML_FILE_EXTENSION = "yaml";
+    static final String JSON_FILE_EXTENSION = "json";
+    static final String YML_FILE_EXTENSION = "yml";
+    static final String YAML_FILE_EXTENSION = "yaml";
 
     public static final String JSON_FILE_NAME_SUFFIX = "." + JSON_FILE_EXTENSION;
     public static final String YML_FILE_NAME_SUFFIX = "." + YML_FILE_EXTENSION;
     public static final String YAML_FILE_NAME_SUFFIX = "." + YAML_FILE_EXTENSION;
-
-    private static final String MAIN_SWAGGER_FILE_NAME = "swagger";
-    public static final String MAIN_SWAGGER_JSON_FILE = MAIN_SWAGGER_FILE_NAME + JSON_FILE_NAME_SUFFIX;
-    public static final String MAIN_SWAGGER_YML_FILE = MAIN_SWAGGER_FILE_NAME + YML_FILE_NAME_SUFFIX;
-    public static final String MAIN_SWAGGER_YAML_FILE = MAIN_SWAGGER_FILE_NAME + YAML_FILE_NAME_SUFFIX;
 
     public static final String CARET = "<caret>";
 }
