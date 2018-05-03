8e0fe0069e16b11b8a552530f546cc3468cb3dd3
redmachine2 <jlynn911@gmail.com>
2018-05-03 16:52:13 -0500

    update FileDetector hasVersion to use Optionals as getLastChild and getText can return null verify that swagger version is "2.0" verify that openapi version is using semantic versioning and starts with 3 update test files to also follow spec

--- src/test/java/org/zalando/intellij/swagger/validator/ValidFileTest.java
+++ src/test/java/org/zalando/intellij/swagger/validator/ValidFileTest.java
@@ -17,4 +17,14 @@ public class ValidFileTest extends SwaggerLightCodeInsightFixtureTestCase {
         doTest("not_swagger.json");
         doTest("not_swagger.yaml");
     }
+
+    public void testSwaggerRootNoVersionIsNotASwaggerFile(){
+        doTest("swagger-root-no-version.json");
+        doTest("swagger-root-no-version.yaml");
+    }
+
+    public void testOpenApiRootNoVersionIsNotOpenApiFile(){
+        doTest("openapi-root-no-version.json");
+        doTest("openapi-root-no-version.yaml");
+    }
 }
