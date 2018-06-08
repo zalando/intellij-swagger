83ae2b2fce5651fe63b10d89f36824269da82e12
Sebastian Monte <sebastian.h.monte@gmail.com>
2018-06-08 14:01:22 +0300

    Devided test resources to smaller directories

--- src/test/java/org/zalando/intellij/swagger/validator/UnknownFieldTest.java
+++ src/test/java/org/zalando/intellij/swagger/validator/UnknownFieldTest.java
@@ -5,91 +5,91 @@ import org.zalando.intellij.swagger.SwaggerLightCodeInsightFixtureTestCase;
 public class UnknownFieldTest extends SwaggerLightCodeInsightFixtureTestCase {
 
     private void doTest(final String fileName) {
-        myFixture.testHighlighting(true, false, false, "validator/field/" + fileName);
+        myFixture.testHighlighting(true, false, false, "validator/field/unknown/" + fileName);
     }
 
     public void testUnknownRootField() {
-        doTest("unknown_root.json");
-        doTest("unknown_root.yaml");
+        doTest("json/unknown_root.json");
+        doTest("yaml/unknown_root.yaml");
     }
 
     public void testUnknownContactField() {
-        doTest("unknown_contact.json");
-        doTest("unknown_contact.yaml");
+        doTest("json/unknown_contact.json");
+        doTest("yaml/unknown_contact.yaml");
     }
 
     public void testUnknownSchemaField() {
-        doTest("unknown_schema.json");
-        doTest("unknown_schema.yaml");
+        doTest("json/unknown_schema.json");
+        doTest("yaml/unknown_schema.yaml");
     }
 
     public void testUnknownSchemaItemsField() {
-        doTest("unknown_schema_items.json");
-        doTest("unknown_schema_items.yaml");
+        doTest("json/unknown_schema_items.json");
+        doTest("yaml/unknown_schema_items.yaml");
     }
 
     public void testUnknownExternalDocsField() {
-        doTest("unknown_external_docs.json");
-        doTest("unknown_external_docs.yaml");
+        doTest("json/unknown_external_docs.json");
+        doTest("yaml/unknown_external_docs.yaml");
     }
 
     public void testUnknownHeaderField() {
-        doTest("unknown_header.json");
-        doTest("unknown_header.yaml");
+        doTest("json/unknown_header.json");
+        doTest("yaml/unknown_header.yaml");
     }
 
     public void testUnknownInfoField() {
-        doTest("unknown_info.json");
-        doTest("unknown_info.yaml");
+        doTest("json/unknown_info.json");
+        doTest("yaml/unknown_info.yaml");
     }
 
     public void testUnknownItemsField() {
-        doTest("unknown_parameter_items.json");
-        doTest("unknown_parameter_items.yaml");
+        doTest("json/unknown_parameter_items.json");
+        doTest("yaml/unknown_parameter_items.yaml");
     }
 
     public void testUnknownLicenseField() {
-        doTest("unknown_license.json");
-        doTest("unknown_license.yaml");
+        doTest("json/unknown_license.json");
+        doTest("yaml/unknown_license.yaml");
     }
 
     public void testUnknownOperationField() {
-        doTest("unknown_operation.json");
-        doTest("unknown_operation.yaml");
+        doTest("json/unknown_operation.json");
+        doTest("yaml/unknown_operation.yaml");
     }
 
     public void testUnknownParametersField() {
-        doTest("unknown_parameters.json");
-        doTest("unknown_parameters.yaml");
+        doTest("json/unknown_parameters.json");
+        doTest("yaml/unknown_parameters.yaml");
     }
 
     public void testUnknownPathField() {
-        doTest("unknown_path.json");
-        doTest("unknown_path.yaml");
+        doTest("json/unknown_path.json");
+        doTest("yaml/unknown_path.yaml");
     }
 
     public void testUnknownResponseField() {
-        doTest("unknown_response.json");
-        doTest("unknown_response.yaml");
+        doTest("json/unknown_response.json");
+        doTest("yaml/unknown_response.yaml");
     }
 
     public void testUnknownResponsesField() {
-        doTest("unknown_responses.json");
-        doTest("unknown_responses.yaml");
+        doTest("json/unknown_responses.json");
+        doTest("yaml/unknown_responses.yaml");
     }
 
     public void testUnknownSecurityDefinitionsField() {
-        doTest("unknown_security_definitions.json");
-        doTest("unknown_security_definitions.yaml");
+        doTest("json/unknown_security_definitions.json");
+        doTest("yaml/unknown_security_definitions.yaml");
     }
 
     public void testUnknownTagsField() {
-        doTest("unknown_tags.json");
-        doTest("unknown_tags.yaml");
+        doTest("json/unknown_tags.json");
+        doTest("yaml/unknown_tags.yaml");
     }
 
     public void testUnknownXmlField() {
-        doTest("unknown_xml.json");
-        doTest("unknown_xml.yaml");
+        doTest("json/unknown_xml.json");
+        doTest("yaml/unknown_xml.yaml");
     }
 }
--- src/test/java/org/zalando/intellij/swagger/validator/json/UnusedRefTest.java
+++ src/test/java/org/zalando/intellij/swagger/validator/json/UnusedRefTest.java
@@ -10,55 +10,55 @@ public class UnusedRefTest extends SwaggerLightCodeInsightFixtureTestCase {
     }
 
     public void testUnusedDefinitionMainFile() {
-        doTest("unused_definition_main_file.json");
+        doTest("unused_definition/json/unused_definition_main_file.json");
     }
 
     public void testUnusedParameterMainFile() {
-        doTest("unused_parameter_main_file.json");
+        doTest("unused_parameter/json/unused_parameter_main_file.json");
     }
 
     public void testUnusedResponseMainFile() {
-        doTest("unused_response_main_file.json");
+        doTest("unused_response/json/unused_response_main_file.json");
     }
 
     public void testUnusedDefinitionWhereDefinitionsAreInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition_in_root.json", "definitions.json");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition_in_root_swagger.json", "swagger.json");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition/json/unused_definition_in_root.json", "definitions.json");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition/json/unused_definition_in_root_swagger.json", "swagger.json");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
         myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedParameterWhereParametersAreInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter_in_root.json", "parameters.json");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter_in_root_swagger.json", "swagger.json");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter/json/unused_parameter_in_root.json", "parameters.json");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter/json/unused_parameter_in_root_swagger.json", "swagger.json");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
         myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedResponseWhereResponsesAreInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response_in_root.json", "responses.json");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response_in_root_swagger.json", "swagger.json");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response/json/unused_response_in_root.json", "responses.json");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response/json/unused_response_in_root_swagger.json", "swagger.json");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
         myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedDefinitionWhereDefinitionsAreNotInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition_not_in_root.json", "definitions.json");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition_not_in_root_swagger.json", "swagger.json");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition/json/unused_definition_not_in_root.json", "definitions.json");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition/json/unused_definition_not_in_root_swagger.json", "swagger.json");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
         myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedParameterWhereParametersAreNotInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter_not_in_root.json", "parameters.json");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter_not_in_root_swagger.json", "swagger.json");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter/json/unused_parameter_not_in_root.json", "parameters.json");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter/json/unused_parameter_not_in_root_swagger.json", "swagger.json");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
         myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedResponseWhereResponsesAreNotInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response_not_in_root.json", "responses.json");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response_not_in_root_swagger.json", "swagger.json");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response/json/unused_response_not_in_root.json", "responses.json");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response/json/unused_response_not_in_root_swagger.json", "swagger.json");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
         myFixture.testHighlighting(true, false, true, virtualFile);
     }
