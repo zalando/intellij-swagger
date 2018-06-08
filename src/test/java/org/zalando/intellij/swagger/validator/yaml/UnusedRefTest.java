0134598c42a1421a9014743a9d9115ed1c1e1905
Sebastian Monte <sebastian.h.monte@gmail.com>
2018-06-08 14:01:22 +0300

    Devided test resources to smaller directories

--- src/test/java/org/zalando/intellij/swagger/validator/yaml/UnusedRefTest.java
+++ src/test/java/org/zalando/intellij/swagger/validator/yaml/UnusedRefTest.java
@@ -6,60 +6,61 @@ import org.zalando.intellij.swagger.SwaggerLightCodeInsightFixtureTestCase;
 public class UnusedRefTest extends SwaggerLightCodeInsightFixtureTestCase {
 
     private void doTest(final String fileName) {
-        myFixture.testHighlighting(true, true, true, "validator/field/" + fileName);
+        myFixture.testHighlighting(true, false, true, "validator/field/" + fileName);
     }
 
     public void testUnusedDefinitionMainFile() {
-        doTest("unused_definition_main_file.yaml");
+        doTest("unused_definition/yaml/unused_definition_main_file.yaml");
     }
 
     public void testUnusedParameterMainFile() {
-        doTest("unused_parameter_main_file.yaml");
+        doTest("unused_parameter/yaml/unused_parameter_main_file.yaml");
     }
 
     public void testUnusedResponseMainFile() {
-        doTest("unused_response_main_file.yaml");
+        doTest("unused_response/yaml/unused_response_main_file.yaml");
     }
 
     public void testUnusedDefinitionWhereDefinitionsAreInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition_in_root.yaml", "definitions.yaml");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition_in_root_swagger.yaml", "swagger.yaml");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition/yaml/unused_definition_in_root.yaml", "definitions.yaml");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition/yaml/unused_definition_in_root_swagger.yaml", "swagger.yaml");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
-        myFixture.testHighlighting(true, true, true, virtualFile);
+        myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedParameterWhereParametersAreInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter_in_root.yaml", "parameters.yaml");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter_in_root_swagger.yaml", "swagger.yaml");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter/yaml/unused_parameter_in_root.yaml", "parameters.yaml");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter/yaml/unused_parameter_in_root_swagger.yaml", "swagger.yaml");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
-        myFixture.testHighlighting(true, true, true, virtualFile);
+        myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedResponseWhereResponsesAreInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response_in_root.yaml", "responses.yaml");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response_in_root_swagger.yaml", "swagger.yaml");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response/yaml/unused_response_in_root.yaml", "responses.yaml");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response/yaml/unused_response_in_root_swagger.yaml", "swagger.yaml");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
-        myFixture.testHighlighting(true, true, true, virtualFile);
+        myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedDefinitionWhereDefinitionsAreNotInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition_not_in_root.yaml", "definitions.yaml");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition_not_in_root_swagger.yaml", "swagger.yaml");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_definition/yaml/unused_definition_not_in_root.yaml", "definitions.yaml");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_definition/yaml/unused_definition_not_in_root_swagger.yaml", "swagger.yaml");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
-        myFixture.testHighlighting(true, true, true, virtualFile);
+        myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedParameterWhereParametersAreNotInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter_not_in_root.yaml", "parameters.yaml");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter_not_in_root_swagger.yaml", "swagger.yaml");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_parameter/yaml/unused_parameter_not_in_root.yaml", "parameters.yaml");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_parameter/yaml/unused_parameter_not_in_root_swagger.yaml", "swagger.yaml");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
-        myFixture.testHighlighting(true, true, true, virtualFile);
+        myFixture.testHighlighting(true, false, true, virtualFile);
     }
 
     public void testUnusedResponseWhereResponsesAreNotInRoot() {
-        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response_not_in_root.yaml", "responses.yaml");
-        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response_not_in_root_swagger.yaml", "swagger.yaml");
+        final VirtualFile virtualFile = myFixture.copyFileToProject("validator/field/unused_response/yaml/unused_response_not_in_root.yaml", "responses.yaml");
+        final VirtualFile swaggerFile = myFixture.copyFileToProject("validator/field/unused_response/yaml/unused_response_not_in_root_swagger.yaml", "swagger.yaml");
         myFixture.configureFromExistingVirtualFile(swaggerFile);
-        myFixture.testHighlighting(true, true, true, virtualFile);
+        myFixture.testHighlighting(true, false, true, virtualFile);
     }
+
 }
