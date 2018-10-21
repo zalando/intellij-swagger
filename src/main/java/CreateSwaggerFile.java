970da32acfd3cb4d6225609544149a00341987bc
Sebastian Monte <sebastian.h.monte@gmail.com>
2018-10-21 20:31:15 +0300

    Added a template for new Swagger/OpenAPI files
    This commit introduces a feature that allows users
    to create a new Swagger/OpenAPI specification file
    from the context menu (in "New").
    
    Fixes #33

---
+++ src/main/java/CreateSwaggerFile.java
@@ -0,0 +1,29 @@
+import com.intellij.ide.actions.CreateFileFromTemplateAction;
+import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.json.JsonFileType;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+import org.jetbrains.yaml.YAMLFileType;
+
+public class CreateSwaggerFile extends CreateFileFromTemplateAction implements DumbAware {
+
+    public CreateSwaggerFile() {
+        super("Swagger/OpenAPI File", "Create a Swagger or an OpenAPI file from the specified template", null);
+    }
+
+    @Override
+    protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder) {
+        builder
+                .setTitle("New API Specification")
+                .addKind("Swagger file (YAML)", YAMLFileType.YML.getIcon(), "Swagger File (YAML).yaml")
+                .addKind("Swagger file (JSON)", JsonFileType.INSTANCE.getIcon(), "Swagger File (JSON).json")
+                .addKind("OpenAPI file (YAML)", YAMLFileType.YML.getIcon(), "OpenAPI File (YAML).yaml")
+                .addKind("OpenAPI file (JSON)", JsonFileType.INSTANCE.getIcon(), "OpenAPI File (JSON).json");
+    }
+
+    @Override
+    protected String getActionName(PsiDirectory directory, String newName, String templateName) {
+        return "Create API Specification " + newName;
+    }
+}
