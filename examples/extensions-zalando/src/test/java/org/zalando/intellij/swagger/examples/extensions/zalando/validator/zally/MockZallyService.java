3c7b36736e9d1a2aa35b3f50ae759d3059e95629
Sebastian Monte <sebastian.h.monte@gmail.com>
2018-10-23 12:41:09 +0300

    Fixed Zally error PSI element fallback
    The element lookup did not work correctly when looking for
    `swagger` element. This commit fixes the issue and adds tests
    to both cases; an error with a matching PSI element and a fallback to root element.

--- examples/extensions-zalando/src/test/java/org/zalando/intellij/swagger/examples/extensions/zalando/validator/zally/MockZallyService.java
+++ examples/extensions-zalando/src/test/java/org/zalando/intellij/swagger/examples/extensions/zalando/validator/zally/MockZallyService.java
@@ -10,7 +10,8 @@ public class MockZallyService implements ZallyService {
 
     public LintingResponse lint(final String spec) {
         final List<Violation> violations = ImmutableList.of(
-                new Violation("Violation 1", "Description", "MUST", "Violation link", "a/path")
+                new Violation("Violation 1", "Description", "MUST", "Violation link", "a/path"),
+                new Violation("Violation 2", "Title", "MUST", "Violation link", "/info/title")
         );
 
         return new LintingResponse(violations);
