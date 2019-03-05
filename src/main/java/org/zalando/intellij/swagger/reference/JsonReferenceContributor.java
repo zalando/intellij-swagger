6059e543c571f9339e675cc5cce4acc2bb488265
Sebastian Monte <sebastian.h.monte@gmail.com>
2019-03-05 13:21:08 +0200

    Use constants for duplicate String patterns

--- src/main/java/org/zalando/intellij/swagger/reference/JsonReferenceContributor.java
+++ src/main/java/org/zalando/intellij/swagger/reference/JsonReferenceContributor.java
@@ -7,11 +7,17 @@ import com.intellij.json.psi.JsonLiteral;
 import com.intellij.json.psi.JsonProperty;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.patterns.StandardPatterns;
+import com.intellij.patterns.StringPattern;
 import com.intellij.psi.PsiReferenceRegistrar;
 import org.jetbrains.annotations.NotNull;
-import org.zalando.intellij.swagger.file.FileConstants;
 
 public class JsonReferenceContributor extends ReferenceContributor {
+
+  private static final StringPattern URL_PATTERN =
+      StandardPatterns.string().matches("^\"https://(.)*");
+
+  private static final StringPattern JSON_FILE_NAME_PATTERN =
+      StandardPatterns.string().matches("(.)*.json(.)*");
 
   @Override
   public void registerReferenceProviders(@NotNull final PsiReferenceRegistrar registrar) {
@@ -29,8 +35,8 @@ public class JsonReferenceContributor extends ReferenceContributor {
                 .withParent(psiElement(JsonProperty.class).withName(SwaggerConstants.REF_KEY)),
             psiElement().withSuperParent(3, psiElement(JsonProperty.class).withName("mapping")))
         .withText(StandardPatterns.string().contains(SwaggerConstants.REFERENCE_PREFIX))
-        .withoutText(StandardPatterns.string().contains(FileConstants.JSON_FILE_NAME_SUFFIX))
-        .withoutText(StandardPatterns.string().matches("^\"https://(.)*"))
+        .withoutText(JSON_FILE_NAME_PATTERN)
+        .withoutText(URL_PATTERN)
         .withLanguage(JsonLanguage.INSTANCE);
   }
 
@@ -38,15 +44,15 @@ public class JsonReferenceContributor extends ReferenceContributor {
     return psiElement(JsonLiteral.class)
         .withSuperParent(3, psiElement(JsonProperty.class).withName("mapping"))
         .withoutText(StandardPatterns.string().contains(SwaggerConstants.REFERENCE_PREFIX))
-        .withoutText(StandardPatterns.string().contains(FileConstants.JSON_FILE_NAME_SUFFIX))
-        .withoutText(StandardPatterns.string().matches("^\"https://(.)*"))
+        .withoutText(JSON_FILE_NAME_PATTERN)
+        .withoutText(URL_PATTERN)
         .withLanguage(JsonLanguage.INSTANCE);
   }
 
   private PsiElementPattern.Capture<JsonLiteral> filePattern() {
     return psiElement(JsonLiteral.class)
-        .withText(StandardPatterns.string().matches("(.)*.json(.)*"))
-        .withoutText(StandardPatterns.string().matches("^\"https://(.)*"))
+        .withText(JSON_FILE_NAME_PATTERN)
+        .withoutText(URL_PATTERN)
         .withLanguage(JsonLanguage.INSTANCE);
   }
 }
