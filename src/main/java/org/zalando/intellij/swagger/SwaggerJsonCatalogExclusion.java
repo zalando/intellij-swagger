e666064cfc61b6b093236b4096e289d796fc0f6f
Sebastian Monte <sebastian.h.monte@gmail.com>
2019-05-08 13:39:21 +0300

    Fix IndexNotReadyException
    Before checking for exclusions for JSON Schema,
    verify that the index is ready.
    
    Fixes #266

--- src/main/java/org/zalando/intellij/swagger/SwaggerJsonCatalogExclusion.java
+++ src/main/java/org/zalando/intellij/swagger/SwaggerJsonCatalogExclusion.java
@@ -6,19 +6,23 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.jsonSchema.remote.JsonSchemaCatalogExclusion;
 import org.jetbrains.annotations.NotNull;
 import org.zalando.intellij.swagger.index.IndexService;
-import org.zalando.intellij.swagger.index.openapi.OpenApiIndexService;
-import org.zalando.intellij.swagger.index.swagger.SwaggerIndexService;
 
 public class SwaggerJsonCatalogExclusion implements JsonSchemaCatalogExclusion {
 
-  private final IndexService indexService =
-      new IndexService(new OpenApiIndexService(), new SwaggerIndexService());
+  private final IndexService indexService;
+  private final ProjectManager projectManager;
+
+  public SwaggerJsonCatalogExclusion(
+      final IndexService indexService, final ProjectManager projectManager) {
+    this.indexService = indexService;
+    this.projectManager = projectManager;
+  }
 
   @Override
   public boolean isExcluded(@NotNull VirtualFile file) {
-    final Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+    final Project[] openProjects = projectManager.getOpenProjects();
 
-    if (openProjects.length > 0) {
+    if (openProjects.length > 0 && indexService.isIndexReady(openProjects[0])) {
       return indexService.isMainSpecFile(file, openProjects[0]);
     }
 
--- src/main/java/org/zalando/intellij/swagger/index/IndexService.java
+++ src/main/java/org/zalando/intellij/swagger/index/IndexService.java
@@ -6,17 +6,21 @@ import com.intellij.psi.PsiFile;
 import java.util.Optional;
 import org.zalando.intellij.swagger.index.openapi.OpenApiIndexService;
 import org.zalando.intellij.swagger.index.swagger.SwaggerIndexService;
+import org.zalando.intellij.swagger.service.intellij.DumbService;
 
 public class IndexService {
 
   private final OpenApiIndexService openApiIndexService;
   private final SwaggerIndexService swaggerIndexService;
+  private final DumbService dumbService;
 
   public IndexService(
       final OpenApiIndexService openApiIndexService,
-      final SwaggerIndexService swaggerIndexService) {
+      final SwaggerIndexService swaggerIndexService,
+      final DumbService dumbService) {
     this.openApiIndexService = openApiIndexService;
     this.swaggerIndexService = swaggerIndexService;
+    this.dumbService = dumbService;
   }
 
   public Optional<VirtualFile> getMainSpecFile(final PsiFile psiFile) {
@@ -38,5 +42,9 @@ public class IndexService {
   public boolean isMainSpecFile(final VirtualFile virtualFile, final Project project) {
     return openApiIndexService.isMainOpenApiFile(virtualFile, project)
         || swaggerIndexService.isMainSwaggerFile(virtualFile, project);
+  }
+
+  public boolean isIndexReady(final Project project) {
+    return !dumbService.isDumb(project);
   }
 }
