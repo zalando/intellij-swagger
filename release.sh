ac3f5c0a10513e2a530eb9a7a4e7bfe73f235e11
asaarilahti <asaarilahti@users.noreply.github.com>
2023-04-13 10:59:29 +0300

    Allow to provide release channel in the release script.

--- release.sh
+++ release.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
-set -eux
+set -euo pipefail
 
-if [ -z "$1" ]
+if [ -z "${1:-}" ]
   then
-    echo "Missing version for master branch"
+    echo "Version needs to be provided."
+    echo "Usage: ./release.sh <version> <release-channel>"
     exit 1
 fi
 
-export $(grep -v '^\#' .env)
-if [ -z "$JETBRAINS_HUB_TOKEN" ]
+if [[ "${2:-}" != "stable" && "${2:-}" != "beta" ]]
+  then
+    echo "Release channel needs to be provided - supported values: stable, beta"
+    echo "Usage: ./release.sh <version> <release-channel>"
+    exit 1
+fi
+
+
+ENVS=$(grep -v '^\#' .env || true)
+if [[ -n "${ENVS}" ]]
+  then
+    export "${ENVS?}"
+fi
+
+if [ -z "${JETBRAINS_HUB_TOKEN:-}" ]
   then
     echo "Please put JETBRAINS_HUB_TOKEN in .env: JETBRAINS_HUB_TOKEN=<token>"
     exit 1
@@ -16,10 +30,10 @@ fi
 
 NEW_VERSION=$1
 
-sed -i "s/<version>.*<\/version>/<version>$NEW_VERSION<\/version>/g" src/main/resources/META-INF/plugin.xml
+sed -i '' "s/<version>.*<\/version>/<version>$NEW_VERSION<\/version>/g" src/main/resources/META-INF/plugin.xml
 
 git checkout master
-./gradlew publishPlugin -Pversion=$NEW_VERSION -x buildSearchableOptions
+./gradlew publishPlugin -Pversion="${NEW_VERSION}" -PjetbrainsReleaseChannel="${2}" -x buildSearchableOptions
 git add src/main/resources/META-INF/plugin.xml
 git commit -m "Bump version"
 
