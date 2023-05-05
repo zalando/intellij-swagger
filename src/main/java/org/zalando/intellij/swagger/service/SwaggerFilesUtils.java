18e57c7164ce9e14012f1c710841a105dff31590
Chris <gross@gross-it.com>
2023-05-05 17:07:28 +0200

    Fixes blank built-in preview in 2022.x

--- src/main/java/org/zalando/intellij/swagger/service/SwaggerFilesUtils.java
+++ src/main/java/org/zalando/intellij/swagger/service/SwaggerFilesUtils.java
@@ -1,15 +1,16 @@
 package org.zalando.intellij.swagger.service;
 
-import com.intellij.util.LocalFileUrl;
 import com.intellij.util.Url;
+import com.intellij.util.Urls;
 import java.io.File;
 import java.nio.file.Path;
+
 import org.jetbrains.annotations.NotNull;
 
 public class SwaggerFilesUtils {
 
   public static Url convertSwaggerLocationToUrl(@NotNull final Path swaggerHtmlDirectory) {
-    return new LocalFileUrl(swaggerHtmlDirectory.toString() + File.separator + "index.html");
+    return Urls.newUri("file", swaggerHtmlDirectory + File.separator + "index.html");
   }
 
   public static boolean isFileReference(final String text) {
